2d3494b477c6df0f17e721de8f8e6965fe1ee6a2
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-21 22:15:49 -0400

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neonBlue: "#00ffff",
+        neonPink: "#ff00ff",
+        darkBg: "#0a0a0f",
+        darkCard: "#111118",
+      },
+      fontFamily: {
+        futuristic: ["'Orbitron', sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
