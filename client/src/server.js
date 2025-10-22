3ca9e0a29aafe3685210d088172042adce34554c
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-21 23:26:47 -0400

    Update server.js

--- client/src/server.js
+++ client/src/server.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const server = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:3042",
-  adapter: require("axios/lib/adapters/xhr") // Force browser XHR adapter
+  adapter: require("axios/lib/adapters/xhr") // use browser XHR adapter
 });
 
 export default server;
