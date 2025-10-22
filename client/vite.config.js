475a5864173d653b6b9d5d1c5305f2085209576e
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-21 22:45:38 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,32 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      process: 'process/browser',
-      stream: 'stream-browserify',
-      util: 'util/',
-      buffer: 'buffer/',
-      crypto: 'crypto-browserify',
+      crypto: "crypto-browserify",
+      stream: "stream-browserify",
+      buffer: "buffer",
+      process: "process/browser",
+      util: "util"
     },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: { global: 'globalThis' },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({ buffer: true, process: true }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [rollupNodePolyFill()],
-    },
-  },
+  define: {
+    global: "globalThis"
+  }
 });
