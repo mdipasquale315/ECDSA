8ef07866949571603cab24a11d008499442270bd
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-21 23:12:19 -0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// Polyfill Buffer for ethereum-cryptography
+// Polyfills for ethereum-cryptography
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
+
+import process from "process";
+window.process = process;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
