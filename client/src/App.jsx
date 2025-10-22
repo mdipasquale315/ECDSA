98bd360cf0a00ebe8f31f4dce1650af065b78ada
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-21 22:31:46 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,22 @@
 import { useState } from "react";
 import Wallet from "./Wallet";
 import Transfer from "./Transfer";
+import ParticleBackground from "./ParticleBackground";
 
-function App() {
+export default function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
   const [privateKey, setPrivateKey] = useState("");
 
   return (
-    <div className="app">
-      <Wallet
-        balance={balance}
-        setBalance={setBalance}
-        address={address}
-        setAddress={setAddress}
-        privateKey={privateKey}
-        setPrivateKey={setPrivateKey}
-      />
-      <Transfer
-        setBalance={setBalance}
-        address={address}
-        privateKey={privateKey}
-      />
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      <ParticleBackground />
+      <div className="absolute top-0 left-0 w-full h-full neon-grid -z-10"></div>
+
+      <Wallet setBalance={setBalance} setAddress={setAddress} setPrivateKey={setPrivateKey} />
+      {privateKey && address && (
+        <Transfer privateKey={privateKey} address={address} setBalance={setBalance} />
+      )}
     </div>
   );
 }
-
-export default App;
