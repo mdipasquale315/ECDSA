c72ba612a354f798dbc7bcfa6938f1a0c4803834
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-21 23:27:09 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import Wallet from "./Wallet";
 import Transfer from "./Transfer";
 import ParticleBackground from "./ParticleBackground";
+import { useState } from "react";
 
 export default function App() {
   const [balance, setBalance] = useState(0);
@@ -9,10 +9,8 @@ export default function App() {
   const [privateKey, setPrivateKey] = useState("");
 
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden bg-black">
       <ParticleBackground />
-      <div className="absolute top-0 left-0 w-full h-full neon-grid -z-10"></div>
-
       <Wallet setBalance={setBalance} setAddress={setAddress} setPrivateKey={setPrivateKey} />
       {privateKey && address && (
         <Transfer privateKey={privateKey} address={address} setBalance={setBalance} />
