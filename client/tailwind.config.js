7f88e91057eee0f2cb154c9bbccb37e1442ed9b2
mdipasquale315 <31933669+mdipasquale315@users.noreply.github.com>
2025-10-21 22:26:35 -0400

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,9 +8,7 @@ export default {
         darkBg: "#0a0a0f",
         darkCard: "#111118",
       },
-      fontFamily: {
-        futuristic: ["'Orbitron', sans-serif"],
-      },
+      fontFamily: { futuristic: ["'Orbitron', sans-serif"] },
     },
   },
   plugins: [],
